c209a924ade04d86ca1061967119cb273547f43b
Shmuel Zeigerman <solomuz0@gmail.com>
2023-01-14 00:56:24 +0200

    Improve 'Plugins Control' utility

--- Macros/scripts/Common/plugins.lua
+++ Macros/scripts/Common/plugins.lua
@@ -1,55 +1,64 @@
 -- started    : 2011-02-20
 -- far2l port : 2023-01-11
 
+local F=far.Flags
+
+local breakkeys = {
+  { BreakKey="RETURN", command="load",       success="Loaded",       fail="Failed to load" },
+  { BreakKey="INSERT", command="forcedload", success="Force-loaded", fail="Failed to force-load" },
+  { BreakKey="DELETE", command="unload",     success="Unloaded",     fail="Failed to unload" },
+}
+
+local properties = {
+  Title="Load/Unload Plugins", Bottom="Enter=load, Ins=force-load, Del=unload",
+}
+
 local function Main()
-
-  local F=far.Flags
-
   -- Get space for this script's data. Kept alive between the script's invocations.
   local ScriptId = "263e6208-e5b2-4bf7-8953-59da207279c7"
-  if not rawget(_G, ScriptId) then rawset(_G, ScriptId, {}) end
-  local T = _G[ScriptId]
-
-  -- A table for plugins' data. Each plugin is keyed here by its GUID.
-  T.plugins = T.plugins or {}
-
-  -- Reset all handles, as they may be closed already.
-  for k,v in pairs(T.plugins) do v.handle = nil end
-
-  -- Update plugins' data with the fresh info.
-  for _, handle in ipairs(far.GetPlugins()) do
-    local info = far.GetPluginInformation(handle)
-    info.handle = handle
-    T.plugins[info.ModuleName] = info
+  local first
+  if not rawget(_G, ScriptId) then
+    rawset(_G, ScriptId, {})
+    first = true
   end
+  local Plugins = _G[ScriptId]
 
-  -- Create menu items.
-  local items = {}
-  for k,v in pairs(T.plugins) do
-    items[#items+1] = {
-      text = v.PInfo.PluginMenu[1] or v.ModuleName:match("[^/]+$"),
-      info = v,
-      handle = v.handle,
-      grayed = not v.handle,
-    }
+  if first then
+    for _, handle in ipairs(far.GetPlugins()) do
+      local info = far.GetPluginInformation(handle)
+      info.handle = handle
+      Plugins[info.ModuleName] = info
+    end
   end
-
-  -- Sort menu items alphabetically.
-  table.sort(items,
-    function(a,b) return win.CompareString(a.text, b.text, nil, "cS") < 0 end)
-
-  local breakkeys = {
-    { BreakKey="RETURN", command="load", success="Loaded", fail="Failed to load" },
-    { BreakKey="INSERT", command="forcedload", success="Force-loaded", fail="Failed to force-load" },
-    { BreakKey="DELETE", command="unload", success="Unloaded", fail="Failed to unload" },
-    { BreakKey="F3", command="info" },
-  }
-
-  local properties = {
-    Title="Load/Unload Plugins", Bottom="Enter=load, Ins=force-load, Del=unload",
-  }
 
   while true do
+    -- Update plugins' data with the fresh info.
+    for modname,oldinfo in pairs(Plugins) do
+      local handle = far.FindPlugin("PFM_MODULENAME", modname)
+      if handle then
+        local info = far.GetPluginInformation(handle)
+        info.handle = handle
+        Plugins[modname] = info
+      else
+        oldinfo.handle = nil
+      end
+    end
+
+    -- Create menu items.
+    local items = {}
+    for _,v in pairs(Plugins) do
+      local loaded = v.handle and (0 ~= bit64.band(v.Flags, F.FPF_LOADED))
+      items[#items+1] = {
+        text = v.PInfo.PluginMenu[1] or v.ModuleName:match("[^/]+$");
+        info = v;
+        checked  = loaded and "+" or not v.handle and "-" or nil;
+      }
+    end
+
+    -- Sort menu items alphabetically.
+    table.sort(items,
+      function(a,b) return win.CompareString(a.text, b.text, nil, "cS") < 0 end)
+
     local item, pos = far.Menu(properties, items, breakkeys)
     if not item then break end
     properties.SelectIndex = pos
@@ -65,21 +74,14 @@ local function Main()
       result = mItem.handle and true
       mItem.grayed = not result
     elseif bItem.command == "unload" then
-      if mItem.handle then
+      if mItem.info.handle then
         if far.GetPluginId() ~= mItem.info.GInfo.SysID then
-          result = far.UnloadPlugin(mItem.handle)
-          if result then mItem.handle = nil end
-          mItem.grayed = result
+          far.UnloadPlugin(mItem.info.handle)
         else
-          far.Message("\nI'm running this script and cannot unload myself !!!\n",
-                      mItem.text, nil, "w")
+          mf.postmacro(far.Message, "I'm running this script and cannot unload myself !!!", mItem.text, nil, "w")
         end
       end
-    elseif bItem.command == "info" then
-      local loaded = (0 ~= bit64.band(mItem.info.Flags, F.FPF_LOADED))
-      far.Message(loaded and "is loaded" or "is not loaded")
     end
-    --far.Message(result and bItem.success or bItem.fail, mItem.text)
   end
 end
 
