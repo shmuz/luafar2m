5a66ad9a9ef244a7271d114d7c084ed834706611
Shmuel Zeigerman <solomuz0@gmail.com>
2023-03-25 22:07:24 +0300

    Common_Calendar: make it portable between Far3 and far2l

--- Macros/scripts/Third_party/Common_Calendar.lua
+++ Macros/scripts/Third_party/Common_Calendar.lua
@@ -48,7 +48,6 @@ local M=Messages()
 local F,msg = far.Flags,far.SendDlgMessage
 local MSinDay=1000*60*60*24
 local DaysInMonths={[0]=31,31,28,31,30,31,30,31,31,30,31,30,31,[13]=31}
-local Text
 
 local function Today()
 --[[ Возвращает таблицу DateTime с полями:
@@ -69,7 +68,7 @@ local function Leap(DateTime) -- високосный год?
 end
 
 local function IncDay(DateTime,Days)
-  local dt=win.FileTimeToSystemTime(win.SystemTimeToFileTime(DateTime)+Days*MSinDay)
+  local dt=win.FileTimeToSystemTime(win.SystemTimeToFileTime(DateTime)+Days*MSinDay) or DateTime
   DaysInMonths[2]=Leap(dt) and 29 or 28 --### потенциальный или реальный баг
   return dt
 end
@@ -119,6 +118,7 @@ local function DecYear(DateTime) -- убавить 1 год
 end
 
 local function Calendar(DateTime)
+  local sd = require "far2.simpledialog"
   M=Messages()
 --==
   local Current=Today()
@@ -127,96 +127,105 @@ local function Calendar(DateTime)
 --==
 
   local dt=DateTime and win.SystemTimeToFileTime(DateTime) and DateTime or Current
-  local ITic
-  local Items = {}
-  Items[01]={F.DI_DOUBLEBOX,3, 1,32,18,0,0,0,0                  ,M.Title}
-  Items[02]={F.DI_TEXT,    -1, 2, 0, 2,0,0,F.DIF_SEPARATOR,0    ,""     }
-  Items[03]={F.DI_BUTTON,   4, 3, 0, 3,0,0,F.DIF_BTNNOCLOSE,0   ,"<"    } --Год назад
-  Items[04]={F.DI_FIXEDIT, 16, 3,19, 3,0,"9999",F.DIF_MASKEDIT,0,""     } --Год
-  Items[05]={F.DI_BUTTON,  27, 3, 0, 3,0,0,F.DIF_BTNNOCLOSE,0   ,">"    } --Год вперёд
-  Items[06]={F.DI_TEXT,    -1, 4, 0, 4,0,0,F.DIF_SEPARATOR,0    ,""     }
+  local Items = {
+    guid="615d826b-3921-48bb-9cf2-c6d345833855";
+    width=36;
+    {tp="dbox"; text=M.Title},
+    {tp="sep"},
+    {tp="butt";             name="decYear"; btnnoclose=1; x1=4;  text="<";        }, --Год назад
+    {tp="fixedit";          name="Year";     mask="9999"; x1=16; width=4;  y1=""; }, --Год
+    {tp="butt";             name="incYear"; btnnoclose=1; x1=27; text=">"; y1=""; }, --Год вперёд
+    {tp="sep"                                                            },
+  }
   local Months={}
   for m=1,12 do
     Months[m]={}; Months[m]["Text"]=M.Months[m]
   end
-  Items[07]={F.DI_BUTTON,   4, 5, 0,21,0,0,F.DIF_BTNNOCLOSE,0,"<"} --Месяц назад
-  Items[08]={F.DI_COMBOBOX,10, 5,24, 5,0,Months,F.DIF_DROPDOWNLIST,0,""} --Месяц
-  Items[09]={F.DI_BUTTON,  27, 5, 0,21,0,0,F.DIF_BTNNOCLOSE,0,">"} --Месяц вперёд
-  Items[10]={F.DI_TEXT,     0, 6, 0, 6,0,0,F.DIF_SEPARATOR,0,""}
-  Items[11]={F.DI_TEXT,     0,14, 0,14,0,0,F.DIF_SEPARATOR,0,""}
-  local yd=6
+  local Add=table.insert
+  Add(Items,{tp="butt";     name="decMonth"; btnnoclose=1;   x1=4;  text="<";                  }) --Месяц назад
+  Add(Items,{tp="combobox"; name="Month";    dropdownlist=1; x1=10; x2=24; list=Months; y1=""; }) --Месяц
+  Add(Items,{tp="butt";     name="incMonth"; btnnoclose=1;   x1=27; text=">";           y1=""; }) --Месяц вперёд
+  Add(Items,{tp="sep"})
+
   for d=1,7 do
-    Items[#Items+1]={F.DI_TEXT, 5, yd+d, 0, yd+d,0,0,0,0,M.DaysOfWeek[d]}
+    Add(Items,{tp="text"; text=M.DaysOfWeek[d]})
   end
-  local IF=18
+  local IF=#Items
+
   for w=0,5 do
     for d=1,7 do
-      Items[#Items+1]={F.DI_TEXT, 8+w*4, yd+d, 0, yd+d,0,0,0,0,""}
+      Add(Items,{tp="text"; x1=8+w*4; ystep=(d==1 and -6); })
     end
   end
-  Items[61]={F.DI_USERCONTROL,8, 7,31,13,0,0,0,0} --Движение по дням
-  Items[62]={F.DI_FIXEDIT,    7,15,16,15,0,"99.99.9999",F.DIF_MASKEDIT+F.DIF_READONLY,0,""}
-  Items[63]={F.DI_BUTTON,    18,15, 0,15,0,0,0,0,M.Ins} -- Вставить дату
-  Items[64]={F.DI_TEXT,       0,16, 0,16,0,0,F.DIF_SEPARATOR,0,""}
-  Items[65]={F.DI_BUTTON,     0,17, 0,17,1,0,F.DIF_CENTERGROUP,1,M.Ok}
+
+  Add(Items,{tp="user";     name="User"; x1=8; ystep=-6; x2=31; height=7; }) --Движение по дням
+  Add(Items,{tp="sep"})
+  Add(Items,{tp="fixedit";  name="Date"; x1=7; x2=16; mask="99.99.9999"; readonly=1; })
+  Add(Items,{tp="butt";     name="Insert"; x1=18; text=M.Ins; y1="";  }) -- Вставить дату
+  Add(Items,{tp="sep"})
+  Add(Items,{tp="butt"; centergroup=1; default=1; text=M.Ok; focus=1; })
+
+  local Dlg = sd.New(Items)
+  local Pos = Dlg:Indexes()
+  local Text
+  local ITic
 
   local function Rebuild(hDlg,dT)
-    msg(hDlg,F.DM_ENABLEREDRAW ,0                       )
-    msg(hDlg,F.DM_SETTEXT      ,04,tostring(dT.wYear)   )
-    msg(hDlg,F.DM_LISTSETCURPOS,08,{SelectPos=dT.wMonth})
+    msg(hDlg,F.DM_ENABLEREDRAW,0)
+    msg(hDlg,F.DM_SETTEXT,Pos.Year,tostring(dT.wYear))
+    msg(hDlg,F.DM_LISTSETCURPOS,Pos.Month,{SelectPos=dT.wMonth})
     local day=WeekStartDay(FirstDay(dT))
     ITic=nil
     for w=0,5 do
       for d=1,7 do
-        msg(hDlg,F.DM_SETTEXT,IF+w*7+d,("%3s "):format(day.wDay)       )
-        msg(hDlg,F.DM_ENABLE ,IF+w*7+d,day.wMonth==dT.wMonth and 1 or 0)
+        local curpos=IF+w*7+d
+        msg(hDlg,F.DM_ENABLE,curpos,day.wMonth==dT.wMonth and 1 or 0)
         if day.wYear==Current.wYear and day.wMonth==Current.wMonth and day.wDay==Current.wDay then
-          msg(hDlg,F.DM_SETTEXT,IF+w*7+d,("[%2s]"):format(day.wDay))
+          msg(hDlg,F.DM_SETTEXT,curpos,("[%2s]"):format(day.wDay))
           ITic= ITic or w*7+d
         elseif day.wMonth==dT.wMonth and day.wDay==dT.wDay then
-          msg(hDlg,F.DM_SETTEXT,IF+w*7+d,("{%2s}"):format(day.wDay))
+          msg(hDlg,F.DM_SETTEXT,curpos,("{%2s}"):format(day.wDay))
           ITic=day.wMonth==dT.wMonth and w*7+d or ITic
         else
-          msg(hDlg,F.DM_SETTEXT,IF+w*7+d,("%3s " ):format(day.wDay))
+          msg(hDlg,F.DM_SETTEXT,curpos,("%3s "):format(day.wDay))
         end
         day=IncDay(day,1)
       end
     end
-    msg(hDlg,F.DM_SETTEXT     ,62,string.format("%02d.%02d.%4d",dT.wDay,dT.wMonth,dT.wYear))
+    msg(hDlg,F.DM_SETTEXT,Pos.Date,string.format("%02d.%02d.%4d",dT.wDay,dT.wMonth,dT.wYear))
     msg(hDlg,F.DM_ENABLEREDRAW,1)
   end
 
-  local function DlgProc(hDlg,Msg,Param1,Param2)
-    if Msg==F.DN_GETDIALOGINFO then
-      return win.Uuid("615d826b-3921-48bb-9cf2-c6d345833855")
-    elseif Msg==F.DN_INITDIALOG then
+  function Items.proc(hDlg,Msg,Param1,Param2)
+    if Msg==F.DN_INITDIALOG then
       Rebuild(hDlg,dt)
-    elseif Msg==F.DN_BTNCLICK and Param1==63 then
-      Text=msg(hDlg,F.DM_GETTEXT, 62, nil)
+    elseif Msg==F.DN_BTNCLICK and Param1==Pos.Insert then
+      Text=msg(hDlg,F.DM_GETTEXT, Pos.Date)
     elseif Msg==F.DN_BTNCLICK then
-      if     Param1==03 then dt=DecYear(dt)  -- Год назад
-      elseif Param1==05 then dt=IncYear(dt)  -- Год вперёд
-      elseif Param1==07 then dt=DecMonth(dt) -- Месяц назад
-      elseif Param1==09 then dt=IncMonth(dt) -- Месяц вперёд
+      if     Param1==Pos.decYear  then dt=DecYear(dt)  -- Год назад
+      elseif Param1==Pos.incYear  then dt=IncYear(dt)  -- Год вперёд
+      elseif Param1==Pos.decMonth then dt=DecMonth(dt) -- Месяц назад
+      elseif Param1==Pos.incMonth then dt=IncMonth(dt) -- Месяц вперёд
       end
       Rebuild(hDlg,dt)
-    elseif Msg==F.DN_EDITCHANGE and Param1==04 then
+    elseif Msg==F.DN_EDITCHANGE and Param1==Pos.Year then
       local oldY=dt.wYear
-      dt.wYear=tonumber(msg(hDlg,F.DM_GETTEXT, 04, nil))
+      dt.wYear=tonumber(msg(hDlg,F.DM_GETTEXT, Pos.Year))
       if win.SystemTimeToFileTime(dt) then
         Rebuild(hDlg,dt)
       else
+        ---require"far2.lua_explorer"(dt,"dt")
         dt.wYear=oldY
       end
-    elseif Msg==F.DN_EDITCHANGE and Param1==08 then
+    elseif Msg==F.DN_EDITCHANGE and Param1==Pos.Month then
       local oldM=dt.wMonth
-      dt.wMonth=(msg(hDlg,F.DM_LISTGETCURPOS, 08, nil)).SelectPos
+      dt.wMonth=(msg(hDlg,F.DM_LISTGETCURPOS, Pos.Month)).SelectPos
       if not win.SystemTimeToFileTime(dt) then
-        msg(hDlg,F.DM_LISTSETCURPOS,08,{SelectPos=dt.wMonth})
+        msg(hDlg,F.DM_LISTSETCURPOS,Pos.Month,{SelectPos=dt.wMonth})
         dt.wMonth=oldM
       end
       Rebuild(hDlg,dt)
-    elseif Msg==F.DN_KEY and Param1==61 then
+    elseif Msg==F.DN_KEY and Param1==Pos.User then
       if     band(Param2,F.KEY_CTRL+F.KEY_RCTRL)~=0 then
         local key = band(Param2, bnot(F.KEY_CTRL+F.KEY_RCTRL))
         if     key==F.KEY_LEFT  then dt=DecMonth(dt)
@@ -230,7 +239,7 @@ local function Calendar(DateTime)
       elseif Param2==F.KEY_DOWN  then dt=IncDay(dt, 1)
       end
       Rebuild(hDlg,dt)
-    elseif Msg==F.DN_MOUSECLICK and Param1==61 then
+    elseif Msg==F.DN_MOUSECLICK and Param1==Pos.User then
       if Param2.ButtonState==1 then
         local i=math.floor(Param2.MousePositionX/4)*7+Param2.MousePositionY+1
         dt=IncDay(dt,i-ITic)
@@ -239,7 +248,7 @@ local function Calendar(DateTime)
     end
   end
 
-  far.Dialog (-1,-1,36,20,nil,Items,nil,DlgProc)
+  Dlg:Run()
   if Text then mf.print(Text) end
 end
 -------------------------------------------------------------------------------
