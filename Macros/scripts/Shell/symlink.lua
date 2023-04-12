268e8e9be2209a6d6fcac55213c01e1e8c3f9c74
Shmuel Zeigerman <solomuz0@gmail.com>
2023-04-12 23:50:38 +0300

    Refactoring

--- Macros/scripts/Shell/symlink.lua
+++ Macros/scripts/Shell/symlink.lua
@@ -4,17 +4,21 @@ local function CheckSymlink(_,data)
 end
 
 Macro {
-  description="Copy symlink target";
+  description="Show symlink target";
   area="Shell"; key="CtrlShiftL";
   condition=CheckSymlink;
   action=function(data)
-    far.CopyToClipboard(data.SymlinkTarget)
+    local Items = {
+      {"DI_DOUBLEBOX",3,1,60,3, 0,0,0,0,"Symlink Target"},
+      {"DI_EDIT",     5,2,58,2, 0,0,"DIF_READONLY",0,data.SymlinkTarget},
+    }
+    far.Dialog(-1,-1,64,5,nil,Items)
   end;
 }
 
 Macro {
   description="Go to symlink target";
-  area="Shell"; key="CtrlShiftL";
+  area="Shell"; key="CtrlPgDn";
   condition=CheckSymlink;
   action=function(data)
     Plugin.Command(far.GetPluginId(), "goto:"..data.SymlinkTarget)
