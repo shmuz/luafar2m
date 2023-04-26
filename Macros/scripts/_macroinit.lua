9cebe0bc5cc314b3642d64efe378b50ed3aebf16
Shmuel Zeigerman <solomuz0@gmail.com>
2023-04-26 13:53:08 +0300

    Load current plugins only with current Far

--- Macros/scripts/_macroinit.lua
+++ Macros/scripts/_macroinit.lua
@@ -1,14 +1,17 @@
-local installed = far.PluginStartupInfo().ShareDir:match("^/usr/.-/luafar")
-local luafar = installed or os.getenv("HOME").."/luafar2l"
 local run = select(2, ...)
+if run ~= 1 then return end
+
+local home = os.getenv("HOME")
+local installed = far.PluginStartupInfo().ShareDir:match("^/usr/.-/luafar")
+local luafar = installed or home.."/luafar2l"
 
 -- add lua_share to package.path
-if run == 1 and not package.path:find("/lua_share/") then
+if not package.path:find("/lua_share/") then
   package.path = luafar.."/lua_share/?.lua;"..package.path
 end
 
 -- load plugins
-if run == 1 and not installed then
+if os.getenv("FARHOME") == home.."/far2l/_build/install" then
   far.RecursiveSearch(luafar, "*.far-plug-wide",
     function(_, fullpath)
       far.LoadPlugin("PLT_PATH", fullpath)
