0469d90a2b7c380b35ff1332ab80345900b9daca
Shmuel Zeigerman <solomuz0@gmail.com>
2023-08-02 18:38:50 +0300

    Adapt bito.lua to far2m

--- Macros/scripts/Third_party/bito.lua
+++ Macros/scripts/Third_party/bito.lua
@@ -1,9 +1,11 @@
+-- Original author : John Doe
+-- Original URL    : https://forum.farmanager.com/viewtopic.php?t=13283
+
 -- https://bito.ai/
 -- https://github.com/gitbito/CLI/raw/main/version-3.3/BitoCLI.msi
 -- https://github.com/gitbito/CLI/blob/main/version-3.3/bito.exe
 -- https://docs.bito.ai/feature-guides/custom-prompt-templates
 
---local directory = win.GetEnv("FARHOME")
 local linewrap = 80
 
 local F = far.Flags
@@ -12,7 +14,7 @@ local function progress (text, title)
   local len = math.max(text:len(), title and title:len() or 0, 7)
   local items = {
   --[[01]] {F.DI_SINGLEBOX,0,0,len+4,3,0,0,0,                0, title},
-  --[[02]] {F.DI_TEXT,     2,1,    0,1,0,0,0,F.DIF_CENTERGROUP, text},
+  --[[02]] {F.DI_TEXT,     2,1,    0,1,0,0,F.DIF_CENTERGROUP,0, text},
   }
   return far.DialogInit(idProgress, -1, -1, len+4, 3, nil, items, F.FDLG_NONMODAL)
 end
@@ -23,7 +25,7 @@ local idInput = win.Uuid"58DD9ECD-CFFA-472E-BFD7-042295C86CAE"
 local function bito (prompt)
   prompt = prompt or far.InputBox(idInput, _name, _prompt, "bito.ai prompt", nil, nil, nil, F.FIB_NONE)
   if prompt then
-    local root = win.GetEnv("FARLOCALPROFILE").."\\"
+    local root = far.InMyConfig().."/"
     local ctxName = root.."ctx.bito"
     local promptName = root.."prompt.bito"
     local promptFile = assert(io.open(promptName, "w"))
@@ -34,13 +36,12 @@ local function bito (prompt)
     local fileFile = assert(io.open(fileName, "w"))
     fileFile:write(ctx=="" and " " or ctx)
     fileFile:close()
-    local cmd = ('"%s" -c "%s" -f "%s" -p "%s"'):format("bito.exe", ctxName, fileName, promptName)
+    local cmd = ('bito -c "%s" -f "%s" -p "%s"'):format(ctxName, fileName, promptName)
     local flags = F.EF_NONMODAL +F.EF_IMMEDIATERETURN +F.EF_DELETEONLYFILEONCLOSE +F.EF_OPENMODE_USEEXISTING
                  +F.EF_DISABLEHISTORY
     editor.Editor("bito.md", nil, nil, nil, nil, nil, flags, nil, nil, 65001)
     editor.SetTitle(nil, "Fetching response...")
     local hDlg = progress("Waiting for data..")
-
     editor.UndoRedo(nil, F.EUR_BEGIN)
     local ei = editor.GetInfo()
     local s = editor.GetString(nil, ei.TotalLines)
@@ -54,15 +55,7 @@ local function bito (prompt)
     end
     far.Text()
 
-    local pipe = io.popen("cmd /c\""..cmd.."\"", "r")
-    --[[
-    for line in pipe:lines() do
-      editor.SetString(nil, i, line,"\n")
-      editor.InsertString()
-      i = i+1
-      editor.Redraw()
-    end
-    --]]
+    local pipe = io.popen(cmd, "r")
     linewrap = linewrap or ei.WindowSizeX-5
     local autowrap = bit64.band(ei.Options, F.EOPT_AUTOINDENT)~=0
     if autowrap then editor.SetParam(nil, F.ESPT_AUTOINDENT, 0) end
