d19b7ec50923a9d7e39a67e25241fb0be6901ac8
Shmuel Zeigerman <solomuz0@gmail.com>
2023-08-13 19:34:49 +0300

    LF History: correct due to latest FAR changes

--- _lfhistory_/plug/lfhistory.lua
+++ _lfhistory_/plug/lfhistory.lua
@@ -100,7 +100,9 @@ local function IsCtrlPgDn (key) return key=="CtrlPgDn" or key=="RCtrlPgDn" end
 
 local function ExecuteFromCmdLine(str, newwindow)
   panel.SetCmdLine(str)
-  far.MacroPost(newwindow and "Keys('ShiftEnter')" or "Keys('Enter')")
+  far.MacroPost(newwindow and "Keys('ShiftEnter')" or "Keys('Enter')",
+    "KMFLAGS_ENABLEOUTPUT") -- this flag was not needed until some FAR change between 09-12 Aug-23
+    -- (without this flag there are panels drawn on the console upon Ctrl-O press)
 end
 
 local function GetTimeString (filetime)
