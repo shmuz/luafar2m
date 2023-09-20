212d08744bb5fa38a56462e82ae04bcdc3e2259b
Shmuel Zeigerman <solomuz0@gmail.com>
2023-09-20 20:59:52 +0300

    Refactoring

--- Macros/scripts/Misc/menupos.lua
+++ Macros/scripts/Misc/menupos.lua
@@ -2,22 +2,22 @@
 -- far2m only
 
 local F = far.Flags
-local PlugValue = {}
-local CfgValue
+
+MacroData.menupos = MacroData.menupos or {}
+local data = MacroData.menupos
 
 Macro {
   description="Plugin Menu Remember Pos";
   area="Menu"; key="Enter NumEnter";
   condition=function(key) return Menu.Id == far.Guids.PluginsMenuId end;
   action=function()
-    local area, winfo = nil, actl.GetWindowInfo()
+    local winfo = actl.GetWindowInfo()
     if winfo then
-      if     winfo.Type == F.WTYPE_PANELS then area = "Shell"
-      elseif winfo.Type == F.WTYPE_EDITOR then area = "Editor"
-      elseif winfo.Type == F.WTYPE_VIEWER then area = "Viewer"
+      if     winfo.Type == F.WTYPE_PANELS then data.Shell  = Menu.Value
+      elseif winfo.Type == F.WTYPE_EDITOR then data.Editor = Menu.Value
+      elseif winfo.Type == F.WTYPE_VIEWER then data.Viewer = Menu.Value
       end
     end
-    if area then PlugValue[area] = Menu.Value end
     Keys(akey(1))
   end;
 }
@@ -27,7 +27,7 @@ Macro {
   area="Menu"; key="Enter NumEnter";
   condition=function(key) return Menu.Id == far.Guids.PluginsConfigMenuId end;
   action=function()
-    CfgValue = Menu.Value
+    data.Config = Menu.Value
     Keys(akey(1))
   end;
 }
@@ -36,7 +36,7 @@ Macro {
   description="Plugin Menu Select Pos";
   area="Shell Editor Viewer"; key="F11";
   action=function()
-    local V = PlugValue[Area.Current]
+    local V = data[Area.Current]
     Keys("F11")
     if V then Menu.Select(V, 0) end
   end;
@@ -46,7 +46,8 @@ Macro {
   description="Config Menu Select Pos";
   area="Shell"; key="AltShiftF9";
   action=function()
+    local V = data.Config
     Keys("AltShiftF9")
-    if CfgValue then Menu.Select(CfgValue, 0) end
+    if V then Menu.Select(V, 0) end
   end;
 }
