4afb90dca8486f59648811c1766a391a06d22e43
Shmuel Zeigerman <solomuz0@gmail.com>
2023-12-22 14:36:41 +0200

    Change algorithms and make the script portable Far3/far2m

--- Macros/scripts/Shell/visual_compare.lua
+++ Macros/scripts/Shell/visual_compare.lua
@@ -1,32 +1,67 @@
 -- Started: 2017-08-02
+-- Portable Far3/far2m: 2023-12-22
 
--- The algorithm for choosing a file pair for visual comparison:
---     1. Active panel:  either 1 selected or valid current file    --> file1
---        Passive panel: either 1 selected or having the same name  --> file2
---     2. Active panel: 2 selected                                  --> file1, file2
---     3. Active panel: >2 selected and valid current file          --> file1
---        Passive panel: either 1 selected or having the same name  --> file2
+-- The algorithm for choosing a file pair for visual comparison (in descending priorities):
+--     1. Active panel: 2 selected
+--     2. Active panel: 1 selected;  passive panel: 1 selected
+--     3. Active panel: 1 selected;  passive panel: the same name
+--     4. Active panel: has current; passive panel: the same name
 
-local CommonKey = "CtrlShiftF2"
+local SLASH = string.sub(package.config,1,1)
+local FAR3 = SLASH=="\\"
+
+local CommonKey = FAR3 and "CtrlAltF2" or "CtrlShiftF2"
 local Modes = { -- uncomment those modes you will actually use
-  meld = true;
---diff_console = true;
---diff_edit = true;
---diff_view = true;
+    meld = not FAR3;
+    winmerge = FAR3;
+--  diff_console = true;
+--  diff_edit = true;
+--  diff_view = true;
 }
 
 local F = far.Flags
-local function GetCurrentItem(pan)         return panel.GetCurrentPanelItem(pan) end
-local function GetSelectedItem(pan,index)  return panel.GetSelectedPanelItem(pan,index) end
-local function isfile(item)                return not item.FileAttributes:find("d") end
-local function extract_name(s)             return s:match("[^/]+$") end
+local FarCmdsId = FAR3 and "3A2AF458-43E2-4715-AFEA-93D33D56C0C2" or far.GetPluginId()
 
-local function fexist(file)
-  return not (win.GetFileAttr(file) or "d"):find("d")
+local function GetCurrentItem(pan)
+  if FAR3 then return panel.GetCurrentPanelItem(nil,pan)
+  else return panel.GetCurrentPanelItem(pan)
+  end
 end
 
-local function join(s1, s2)
-  return s1=="" and s2 or s1:find("/$") and s1..s2 or s1.."/"..s2
+local function GetSelectedItem(pan,index)
+  if FAR3 then return panel.GetSelectedPanelItem(nil,pan,index)
+  else return panel.GetSelectedPanelItem(pan,index)
+  end
+end
+
+local function GetPanelDirectory(pan)
+  if FAR3 then return panel.GetPanelDirectory(nil,pan).Name
+  else return panel.GetPanelDirectory(pan)
+  end
+end
+
+local function isfile(item)      return not item.FileAttributes:find("d") end
+local function isselected(item)  return bit64.band(item.Flags,F.PPIF_SELECTED) ~= 0 end
+
+local function extract_name(s)
+  if FAR3 then return s:match("[^\\]+$")
+  else return s:match("[^/]+$")
+  end
+end
+
+local function fexist(file)
+  local attr = win.GetFileAttr(file)
+  return attr and not attr:find("d")
+end
+
+local function join(dir, file)
+  return dir=="" and file
+    or dir:find(SLASH.."$") and dir..file
+    or dir..SLASH..file
+end
+
+local function fullname(dir, file)
+  return file:find(SLASH) and file or join(dir, file)
 end
 
 -- collect items, skip directories
@@ -35,92 +70,105 @@ local function Collect(whatPanel, limit)
   local aInfo = panel.GetPanelInfo(whatPanel)
   for k=1,aInfo.SelectedItemsNumber do
     local item = GetSelectedItem(whatPanel,k)
-    if isfile(item) then
+    if isfile(item) and isselected(item) then
       if #selTable < limit then
         table.insert(selTable,item)
       else
-        selTable = {}; break -- too many selected items, use current item
+        selTable = {}; break -- too many selected items, discard all
       end
     end
   end
-  if selTable[1] == nil then
-    local item = GetCurrentItem(whatPanel)
-    if isfile(item) then table.insert(selTable,item) end
-  end
+  local item = GetCurrentItem(whatPanel)
+  if isfile(item) then selTable.Current=item; end
   return selTable
 end
 
-local function Run(mode)
+local function GetPairForCompare()
   local ACT,PSV = 1,0 -- active and passive panels
-  local dirActive  = panel.GetPanelDirectory(ACT)
-  local dirPassive = panel.GetPanelDirectory(PSV)
+  local dirActive  = GetPanelDirectory(ACT)
+  local dirPassive = GetPanelDirectory(PSV)
   local trgActive, trgPassive
-----------------------------------------------------------------------------------------------------
+  --------------------------------------------------------------------------------------------------
   local selAct, selPass = Collect(ACT,2), Collect(PSV,1)
-  if #selAct == 1 then
-    local aItem = selAct[1]
-    trgActive = join(dirActive, aItem.FileName)
-    if #selPass == 1 then
-      local pItem = selPass[1]
-      if bit64.band(pItem.Flags, F.PPIF_SELECTED) ~= 0 then
-        trgPassive = join(dirPassive, pItem.FileName)
-      end
+  if #selAct == 2 then
+    trgActive  = fullname(dirActive, selAct[1].FileName)
+    trgPassive = fullname(dirActive, selAct[2].FileName)
+  elseif #selAct == 1 and #selPass == 1 then
+    trgActive  = fullname(dirActive,  selAct[1].FileName)
+    trgPassive = fullname(dirPassive, selPass[1].FileName)
+  elseif next(selAct) then
+    local name1 = (selAct[1] or selAct.Current).FileName
+    local fullname2 = join(dirPassive, extract_name(name1))
+    if fexist(fullname2) then
+      trgActive  = fullname(dirActive, name1)
+      trgPassive = fullname2
     end
-    if not trgPassive then
-      local trg2 = join(dirPassive, extract_name(aItem.FileName))
-      if fexist(trg2) then trgPassive = trg2 end
-    end
-  elseif #selAct == 2 then
-    local item1, item2 = selAct[1], selAct[2]
-    trgActive = join(dirActive, item1.FileName)
-    trgPassive = join(dirActive, item2.FileName)
+  end
+  return trgActive, trgPassive
+end
+
+local function Run(trgActive, trgPassive)
+  -- prepare the menu
+  local items = {}
+  for k,v in pairs(Modes) do
+    if v then table.insert(items, {Val=k}) end
   end
 
-  if trgActive and trgPassive then
-    if mode == "meld" then
-      local command = ("meld %q %q &"):format(trgActive,trgPassive)
-      os.execute(command)
-
-    elseif mode == "diff_console" then
-      local command = ("diff -u %q %q"):format(trgActive,trgPassive)
-      far.Execute(command)
-      far.AdvControl("ACTL_REDRAWALL")
-      Keys("CtrlO")
-
-    elseif mode == "diff_edit" then
-      local command = ("diff -u %q %q"):format(trgActive,trgPassive)
-      Plugin.Command(far.GetPluginId(), "edit:<"..command)
-
-    elseif mode == "diff_view" then
-      local command = ("diff -u %q %q"):format(trgActive,trgPassive)
-      Plugin.Command(far.GetPluginId(), "view:<"..command)
-
-    end
+  local mode
+  if items[1] == nil then
+    far.Message("No mode is specified", "Error", nil, "w")
+    return
+  elseif items[2] == nil then
+    mode = items[1].Val -- only 1 mode available; no menu needed
   else
-    far.Message("No suitable file pair found", "Visual compare", nil, "w")
+    table.sort(items, function(a,b) return a.Val < b.Val; end)
+    for i,v in ipairs(items) do
+      v.text = ("&%d. %s"):format(i, v.Val)
+    end
+    local item = far.Menu({Title="Select compare mode"},items)
+    if item then mode = item.Val
+    else return
+    end
+  end
+
+  if mode == "meld" then
+    local command = ("meld %q %q &"):format(trgActive,trgPassive)
+    os.execute(command)
+
+  elseif mode == "winmerge" then
+    local command = ("%q %q"):format(trgActive,trgPassive)
+    win.ShellExecute(nil, nil, "winmerge", command)
+
+  elseif mode == "diff_console" then
+    local command = ("diff -u %q %q"):format(trgActive,trgPassive)
+    if FAR3 then
+      panel.GetUserScreen(); win.system(command); panel.SetUserScreen()
+    else
+      far.Execute(command)
+    end
+    far.AdvControl("ACTL_REDRAWALL")
+    Keys("CtrlO")
+
+  elseif mode == "diff_edit" then
+    local command = ("diff -u %q %q"):format(trgActive,trgPassive)
+    Plugin.Command(FarCmdsId, "edit:<"..command)
+
+  elseif mode == "diff_view" then
+    local command = ("diff -u %q %q"):format(trgActive,trgPassive)
+    Plugin.Command(FarCmdsId, "view:<"..command)
+
   end
 end
 
-if Modes.meld then Macro {
-    description="Visual compare (meld)";
-    area="Shell"; key=CommonKey;
-    action=function() Run("meld") end;
-} end
-
-if Modes.diff_console then Macro {
-    description="Visual compare (diff in console)";
-    area="Shell"; key=CommonKey;
-    action=function() Run("diff_console") end;
-} end
-
-if Modes.diff_edit then Macro {
-    description="Visual compare (diff in editor)";
-    area="Shell"; key=CommonKey;
-    action=function() Run("diff_edit") end;
-} end
-
-if Modes.diff_view then Macro {
-    description="Visual compare (diff in viewer)";
-    area="Shell"; key=CommonKey;
-    action=function() Run("diff_view") end;
-} end
+Macro {
+  description="Visual compare";
+  area="Shell"; key=CommonKey;
+  action=function()
+    local file1,file2 = GetPairForCompare()
+    if file1 then
+      Run(file1,file2)
+    else
+      far.Message("No suitable file pair found", "Visual compare", nil, "w")
+    end
+  end;
+}
