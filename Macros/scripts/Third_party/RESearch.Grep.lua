0ac45faa9b1871acf4463a5054321beaa9196f8a
Shmuel Zeigerman <solomuz0@gmail.com>
2024-08-15 20:56:18 +0300

    Refactor RESearch.Grep.lua

--- Macros/scripts/Third_party/RESearch.Grep.lua
+++ Macros/scripts/Third_party/RESearch.Grep.lua
@@ -12,8 +12,10 @@
 -- Author's Github repository: https://github.com/z0hm/far-scripts
 
 local MacroKey = "AltG"
+
 local F = far.Flags
 local EFlags = bit64.bor(F.EF_NONMODAL, F.EF_IMMEDIATERETURN, F.EF_OPENMODE_USEEXISTING, F.EF_DISABLEHISTORY)
+local LinePattern = '^(%d-)[-:](.+)$'
 
 local function OpenEditor(fname, line, col)
   return editor.Editor(fname,nil,nil,nil,nil,nil,EFlags,line,col)
@@ -21,14 +23,17 @@ end
 
 local function GetFileName(l)
   -- [1] /home/shmuel/luafar2m/_build/install/lf4ed/plug/wrap.lua : 3
-  return regex.match(l,'^\\[\\d+?\\] (.+?)(?: : \\d+)?$')
+  local s = l:match("^%[%d+%] (.*)")
+  if s then
+    return s:match("(.*) : %d+$") or s
+  end
 end
 
 local function GInfo()
-  local ei=editor.GetInfo()
+  local ei = editor.GetInfo()
   local y,x,p = ei.CurLine, ei.CurPos, ei.LeftPos
   local l,i,f = editor.GetString(nil,y).StringText, y, nil
-  local n,s = l:match('^(%d-)[-:](.+)$')
+  local n,s = l:match(LinePattern)
   for j = ei.CurLine,1,-1 do
     f = GetFileName(editor.GetString(nil,j).StringText)
     if f then i = j; break; end
@@ -101,7 +106,7 @@ Macro {
       local t = { filename=inf.TargetFName }
       for j=inf.FNameLNum+1, editor.GetInfo().TotalLines do
         local l=editor.GetString(nil,j).StringText
-        local y,s = l:match('^(%d-)[-:](.+)$')
+        local y,s = l:match(LinePattern)
         if y then
           table.insert(t, {y=y; s=s})
         elseif GetFileName(l) then
@@ -117,18 +122,18 @@ Macro {
   description="Grep:  Save all lines in all files";
   area="Editor"; key=MacroKey; filemask="*.tmp";
   action=function()
-    local t={}
+    local t = {}
     for j=1,editor.GetInfo().TotalLines do
-      local l=editor.GetString(nil,j).StringText
+      local l = editor.GetString(nil,j).StringText
       local f = GetFileName(l)
       if f then
         if t[1] then
           FileSave(t)
-          t={}
+          t = {}
         end
         t.filename = f
       elseif t.filename then
-        local y,s = l:match('^(%d-)[-:](.+)$')
+        local y,s = l:match(LinePattern)
         if y then
           table.insert(t, {y=y; s=s})
         end
