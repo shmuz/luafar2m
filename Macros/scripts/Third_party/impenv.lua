3f0fdb1f1e1e81e7b487a2c267e651116f91ad7d
Shmuel Zeigerman <solomuz0@gmail.com>
2024-08-20 23:50:59 +0300

    impenv.lua: changes from the author

--- Macros/scripts/Third_party/impenv.lua
+++ Macros/scripts/Third_party/impenv.lua
@@ -6,13 +6,14 @@
 
 @Contents
 $ Import environment — загрузка *.env файлов
+
  #Функции скрипта#
 
- • §¦Запуск из командной строки по префиксу #env:#.
+ #*# §¦Запуск из командной строки по префиксу #env:#.
    §¦Рекомендуется назначить ассоциацию на #*.env#.
- • §¦При загрузке макросов, может автоматически загружать файл #autoload.env# из
+ #*# §¦При загрузке макросов, может автоматически загружать файл #autoload.env# из
 пользовательской директории (см.)
- • §¦Макрос, позволяющий выбрать файл для импорта из списка.
+ #*# §¦Макрос, позволяющий выбрать файл для импорта из списка.
 Файлы читаются из пользовательской директории.
    §¦Клавиатурную комбинацию для вызова нужно определить через опцию #macrokey#
 (по умолчанию не задана).
@@ -20,10 +21,14 @@ $ Import environment — загрузка *.env файлов
 
  #Пользовательская директория#
 
- По умолчанию - #far.InMyConfig(".env")#.
-Может быть переопределена через опцию #envdir#
-(поддерживается cfgscript/~ScriptsBrowser~@https://forum.farmanager.com/viewtopic.php?f=15&t=10418@).
+ По умолчанию:
+- Windows: #%FARPROFILE%\.env#
+- Linux (far2m): #far.InMyConfig(".env")#
+
  Если эта директория отсутствует, то ищется директория #.env# рядом со скриптом.
+
+ Значение может быть переопределено через опцию #envdir#
+(поддерживается #cfgscript#/~ScriptsBrowser~@https://forum.farmanager.com/viewtopic.php?f=15&t=10418@).
 
 
  #Командная строка#
@@ -32,7 +37,11 @@ $ Import environment — загрузка *.env файлов
    §¦Импорт окружения из заданного файла.
 
  #env:directory#
-   §¦Чтение списка *.env-файлов из заданной директории,
+   §¦Чтение списка *.env-файлов из #заданной# директории,
+и выбор файла для импорта из списка.
+
+ #env:/envdir#
+   §¦Чтение списка *.env-файлов из #пользовательской# директории,
 и выбор файла для импорта из списка.
 
  #env:#
@@ -40,31 +49,35 @@ $ Import environment — загрузка *.env файлов
 
  Примечания:
 
- • §¦При запуске через префикс имя файла/директории ожидаются без кавычек.
- • §¦Возможен также запуск без установки, через #lua:@@impenv.lua#.
+ #*# §¦При запуске через префикс имя файла/директории ожидаются без кавычек.
+ #*# §¦Возможен также запуск без установки, через #lua:@@impenv.lua#.
 В этом случае кавычки обязательны.
- • §¦Поддерживается также запуск через ~LuaShell~@https://forum.farmanager.com/viewtopic.php?f=15&t=10907@.
+ #*# §¦Поддерживается также запуск через ~LuaShell~@https://forum.farmanager.com/viewtopic.php?f=15&t=10907@.
 
 
- #Формат *.env-файлов#
+ #Формат *.env файлов#
 
  #    ##comment#
  #    var=value#
 @
 --]]
 
+local isWindows = not far.InMyConfig
 local Info = Info or package.loaded.regscript or function(...) return ... end --luacheck: ignore 113/Info
 local nfo = Info { _filename or ...,
   name        = "Import environment";
   description = "Choose / import *.env files";
-  version     = "0.1"; --http://semver.org/lang/ru/
+  version     = "0.2"; --https://semver.org/lang/ru/
   author      = "jd";
   url         = "https://forum.farmanager.com/viewtopic.php?t=13400";
   id          = "D23D1FA0-043F-4B77-84FF-6967F87AF481";
+  minfarversion = {3,0,0,6311,0}; -- win.JoinPath (LuaMacro 823)
 
   --disabled    = false;
   options     = {
-    envdir = far.InMyConfig(".env"),
+    envdir = isWindows
+          and win.JoinPath(win.GetEnv("FARPROFILE"), ".env")
+           or far.InMyConfig(".env"),
     macrokey = "none",
   };
 }
@@ -93,6 +106,7 @@ end
 
 local F = far.Flags
 local selfpath = _filename or ...
+local selfdir = selfpath:match(isWindows and "^(.*[\\/])" or "^(.*/)")
 
 local function chooseEnv (dir)
   local items = {}
@@ -139,10 +153,7 @@ end
 
 -- .env directory searched in %FARPROFILE% or near the script
 local function getEnvDir ()
-  for _,dir in ipairs {
-    O.envdir,
-    selfpath:match"^(.*/)"..".env",
-  } do
+  for _,dir in ipairs { O.envdir, selfdir..".env" } do
     local attr = win.GetFileAttr(dir)
     if attr and attr:match("d") then
       return dir
@@ -158,7 +169,9 @@ local function processPath (pathname)
     if envdir then
       chooseEnv(envdir)
     else
-      local msg = (".env directory expected in %s or near the script"):format(far.InMyConfig())
+      local msg = (".env directory expected in %s or near the script"):format(
+        isWindows and win.GetEnv("FARPROFILE") or far.InMyConfig()
+      )
       far.Message(msg, nfo.name, nil, "w")
     end
     return
@@ -192,7 +205,7 @@ if Macro then
   }
   -- autoload env from .env\autoload.env
   local envdir = getEnvDir()
-  local file = envdir and envdir.."/autoload.env"
+  local file = envdir and win.JoinPath(envdir, "autoload.env")
   if file and win.GetFileAttr(file) then
     importEnv(file)
   end
