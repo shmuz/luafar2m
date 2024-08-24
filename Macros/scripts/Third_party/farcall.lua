54e0a81a202430fc4af74c53616f34afac0491c9
Shmuel Zeigerman <solomuz0@gmail.com>
2024-08-24 15:08:51 +0300

    Refactoring

--- Macros/scripts/Third_party/farcall.lua
+++ Macros/scripts/Third_party/farcall.lua
@@ -5,19 +5,20 @@
 local action = function(text)
     local batpath = far.InMyTemp("__farcall")
     local envpath = far.InMyTemp("__farenv")
-    local bat = io.open(batpath, "w")
-    bat:write('cd "', far.GetCurrentDirectory(), '"\n')
-    bat:write(". ", text, '\n') -- 'source' didn't work while '.' did
+    local bat = assert(io.open(batpath, "w"))
+    bat:write('cd "', far.GetCurrentDirectory(), '" || exit 1\n')
+    bat:write(". ", text, ' || exit 2\n') -- 'source' didn't work while '.' did
     bat:write('env > ', envpath, '\n')
     bat:close()
     win.chmod(batpath, tonumber("0774", 8))
 
     panel.GetUserScreen()
-    win.system(batpath)
-    for line in io.lines(envpath) do
-        local name, value = line:match('^([^=]+)=(.*)')
-        if name then
-            win.SetEnv(name, value)
+    if 0 == win.system(batpath) then
+        for line in io.lines(envpath) do
+            local name, value = line:match('^([^=]+)=(.*)')
+            if name then
+                win.SetEnv(name, value)
+            end
         end
     end
     win.DeleteFile(batpath)
