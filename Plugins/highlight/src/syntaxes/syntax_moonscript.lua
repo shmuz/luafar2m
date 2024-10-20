1d2b8ca2326febea5399fe3d55cff7749e71731d
Shmuel Zeigerman <solomuz0@gmail.com>
2024-10-20 18:40:09 +0300

    Update syntax_moonscript.lua

--- Plugins/highlight/src/syntaxes/syntax_moonscript.lua
+++ Plugins/highlight/src/syntaxes/syntax_moonscript.lua
@@ -22,14 +22,14 @@ local syntax_moonscript =
     pattern = [[ == | <= | >= | ~= | != | < | > ]];
   },
   {
-    name = "String1"; fgcolor = "purple"; color_unfinished= "darkblue on purple";
+    name = "String1"; fgcolor = "green"; color_unfinished= "darkblue on purple";
     pat_open     = [[ " ]];
     pat_skip     = [[ (?: \\. | [^\\"] )* ]];
     pat_close    = [[ " ]];
     pat_continue = [[ \\?$ ]];
   },
   {
-    name = "String2"; fgcolor = "purple"; color_unfinished= "darkblue on purple";
+    name = "String2"; fgcolor = "green"; color_unfinished= "darkblue on purple";
     pat_open     = [[ ' ]];
     pat_skip     = [[ (?: \\. | [^\\'] )* ]];
     pat_close    = [[ ' ]];
@@ -41,6 +41,32 @@ local syntax_moonscript =
     pattern = [[ \b(?:
       class|extends|if|then|super|do|with|import|export|while|elseif|return|for|in|from|when|using|
       else|and|or|not|switch|break|continue
+      )\b ]];
+  },
+  {
+    name = "Function"; fgcolor = "purple";
+    pattern = [[ \b(?:
+      _G|_VERSION|assert|collectgarbage|dofile|error|getfenv|getmetatable|ipairs|load|loadfile|loadstring|module|next|
+      pairs|pcall|print|rawequal|rawget|rawset|require|select|setfenv|setmetatable|tonumber|tostring|type|unpack|xpcall|
+      coroutine\.create|coroutine\.resume|coroutine\.running|coroutine\.status|coroutine\.wrap|coroutine\.yield|
+      debug\.debug|debug\.getfenv|debug\.gethook|debug\.getinfo|debug\.getlocal|debug\.getmetatable|debug\.getregistry|
+      debug\.getupvalue|debug\.setfenv|debug\.sethook|debug\.setlocal|debug\.setmetatable|debug\.setupvalue|
+      debug\.traceback|io\.close|io\.flush|io\.input|io\.lines|io\.open|io\.output|io\.popen|io\.read|io\.stderr|
+      io\.stdin|io\.stdout|io\.tmpfile|io\.type|io\.write|math\.abs|math\.acos|math\.asin|math\.atan|math\.atan2|
+      math\.ceil|math\.cos|math\.cosh|math\.deg|math\.exp|math\.floor|math\.fmod|math\.frexp|math\.huge|math\.ldexp|
+      math\.log|math\.log10|math\.max|math\.min|math\.modf|math\.pi|math\.pow|math\.rad|math\.random|math\.randomseed|
+      math\.sin|math\.sinh|math\.sqrt|math\.tan|math\.tanh|os\.clock|os\.date|os\.difftime|os\.execute|os\.exit|
+      os\.getenv|os\.remove|os\.rename|os\.setlocale|os\.time|os\.tmpname|package\.config|package\.cpath|package\.loaded|
+      package\.loaders|package\.loadlib|package\.path|package\.preload|package\.seeall|string\.byte|string\.char|
+      string\.dump|string\.find|string\.format|string\.gmatch|string\.gsub|string\.len|string\.lower|string\.match|
+      string\.rep|string\.reverse|string\.sub|string\.upper|table\.concat|table\.insert|table\.maxn|table\.remove|
+      table\.sort
+      )\b ]];
+  },
+  {
+    name = "Library"; color="darkred on white";
+    pattern = [[ (?<![\w.])(?:
+      coroutine|debug|io|math|os|package|string|table
       )\b ]];
   },
   {
