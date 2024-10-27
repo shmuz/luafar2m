38acbe7800794a20548be44896012de9551dd775
Shmuel Zeigerman <solomuz0@gmail.com>
2024-10-27 21:11:39 +0200

    Refactoring

--- Macros/scripts/Common/ToggleScreens.lua
+++ Macros/scripts/Common/ToggleScreens.lua
@@ -1,9 +1,8 @@
 -- Started: 2024-05-24
--- This utility makes possible switch back and forth between 2 screens.
---   1. Press F12 to see the list of all screens.
---   2. From the macro browser press Enter on the item whose key is "0_Toggle_Screens".
---   3. The Settings dialog appears. Enter the desired screen numbers and press "Enable" button.
---   4. Now you are able to toggle between 2 screens with CtrlTab presses.
+-- This utility makes possible to switch back and forth between 2 screens.
+--   1. Envoke the macro.
+--   2. The Settings dialog appears. Select the desired screens and press "Enable" button.
+--   3. Toggle between 2 screens with CtrlTab.
 
 local Enable
 local Screen1, Screen2
@@ -13,29 +12,43 @@ local Title = "Toggle between 2 screens"
 local function Settings()
   local sd = require "far2.simpledialog"
   local items = {
-    width=50;
+    width=Far.Width; -- maximal width to avoid scrolling file names
     {tp="dbox"; text=Title; },
     {tp="text"; text="Screen 1:" },
-    {tp="fixedit"; mask="99"; width=3; name="scr1"; y1=""; x1=15; text=Screen1 or 0; },
+    {tp="combobox"; dropdown=1; y1=""; x1=15; name="scr1"; list={}; val=Screen1 or 1; },
     {tp="text"; text="Screen 2:" },
-    {tp="fixedit"; mask="99"; width=3; name="scr2"; y1=""; x1=15; text=Screen2 or 0; },
+    {tp="combobox"; dropdown=1; y1=""; x1=15; name="scr2"; list={}; val=Screen2 or 1; },
     {tp="sep"; },
     {tp="butt"; centergroup=1; text="&Enable";  name="enable"; default=1; },
     {tp="butt"; centergroup=1; text="&Disable"; name="disable"; },
+    {tp="butt"; centergroup=1; text="&Cancel"; cancel=1; },
   }
+
   local Dlg = sd.New(items)
-  local Pos = Dlg:Indexes()
+  local Pos, Elem = Dlg:Indexes()
+
+  local wcount = actl.GetWindowCount()
+  if Elem.scr1.val > wcount then Elem.scr1.val = 1 end -- avoid combobox showing empty line
+  if Elem.scr2.val > wcount then Elem.scr2.val = 1 end -- ditto
+
+  for k=1,wcount do -- fill comboboxes
+    local inf = actl.GetWindowInfo(k)
+    local t = { Text = ("%-10s%s"):format(inf.TypeName, inf.Name) }
+    table.insert(Elem.scr1.list, t)
+    table.insert(Elem.scr2.list, t)
+  end
+
   local out, pos = Dlg:Run()
   if out then
-    Screen1 = tonumber(out.scr1) or 0
-    Screen2 = tonumber(out.scr2) or 0
+    Screen1 = out.scr1
+    Screen2 = out.scr2
     Enable = (pos == Pos.enable)
   end
 end
 
-Macro { -- to be called from Macro Browser
+Macro {
   description="Turn ON/OFF toggle 2 selected screens";
-  area="Common"; key="0_Toggle_Screens";
+  area="Common"; key="CtrlAltJ";
   action=function() Settings() end;
 }
 
@@ -46,9 +59,9 @@ Macro {
     return Enable and Screen1 and Screen2
   end;
   action=function()
-    local Pos = actl.GetWindowInfo().Pos - 1
-    Pos = Pos==Screen1 and Screen2 or Screen1
-    if 0 == actl.SetCurrentWindow(Pos + 1, true) then
+    local pos = actl.GetWindowInfo().Pos
+    pos = pos==Screen1 and Screen2 or Screen1
+    if 0 == actl.SetCurrentWindow(pos, true) then
       Settings()
     end
   end;
