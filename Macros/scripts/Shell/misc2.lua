a58c28027291842529e65021dcdf03b351dc011c
Shmuel Zeigerman <solomuz0@gmail.com>
2024-12-04 00:58:21 +0200

    More fixes

--- Macros/scripts/Shell/misc2.lua
+++ Macros/scripts/Shell/misc2.lua
@@ -1,4 +1,5 @@
 local SharedKey = "CtrlAltE"
+local SharedId = "F2199E52-FE6D-45C0-B39B-8C06984EBBE1"
 
 local function ShowDiff (aName)
   local dir = panel.GetPanelDirectory(nil, 1).Name
@@ -13,37 +14,37 @@ local function ShowDiff (aName)
 end
 
 Macro {
-  id="A7618020-A10A-495F-9A0B-E6446E8C4FE2";
+  id=SharedId; key=SharedKey; sortpriority=48;
   description = "git diff: far2m";
-  area="Shell"; key=SharedKey; sortpriority=48;
+  area="Shell";
   action=function() ShowDiff("far2m") end;
 }
 
 Macro {
-  id="3A6AB140-F371-427C-83E0-91163C630859";
+  id=SharedId; key=SharedKey; sortpriority=47;
   description = "git diff: luafar2m";
-  area="Shell"; key=SharedKey; sortpriority=47;
+  area="Shell";
   action=function() ShowDiff("luafar2m") end;
 }
 
 Macro {
-  id="EDD8EDEE-3AB9-44F5-A53C-AD56D07EC72C";
+  id=SharedId; key=SharedKey; sortpriority=46.5;
   description = "git diff: luafar-far2l";
-  area="Shell"; key=SharedKey; sortpriority=46.5;
+  area="Shell";
   action=function() ShowDiff("luafar-far2l") end;
 }
 
 Macro {
-  id="8E850BD6-45C8-4A1E-8E2C-A800E995C4E5";
+  id=SharedId; key=SharedKey; sortpriority=46;
   description = "git diff: scite-config";
-  area="Shell"; key=SharedKey; sortpriority=46;
+  area="Shell";
   action=function() ShowDiff("scite-config") end;
 }
 
 Macro {
-  id="5613CA58-E45D-4231-8860-0826212298CA";
+  id=SharedId;  key=SharedKey; sortpriority=44;
   description = "Generate luacheck_config.lua";
-  area="Shell"; key=SharedKey; sortpriority=44;
+  area="Shell";
   action=function()
     local trg = far.GetMyHome().."/luacheck_config.lua"
     require("far2.luacheck_generate")(trg)
