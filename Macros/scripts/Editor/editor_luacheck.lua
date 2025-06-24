0250f9e8a4fc6d71c082273e3900e43252650a4e
Shmuel Zeigerman <solomuz0@gmail.com>
2025-06-24 12:20:21 +0300

    editor_luacheck: merge with far3 version

--- Macros/scripts/Editor/editor_luacheck.lua
+++ Macros/scripts/Editor/editor_luacheck.lua
@@ -1,28 +1,33 @@
+------------------------------------------------------------------------------------------------
+-- Started:                 2020-09-13
+-- Author:                  Shmuel Zeigerman
+-- Language:                Lua 5.1
+-- Portability:             far3 (>= 3300), far2m
+-- Far plugin:              LuaMacro, LF4Editor
+-- Depends on:              Lua module 'luacheck'
+------------------------------------------------------------------------------------------------
+
 -------- Settings
+local MacroKey = "CtrlShiftF7"
 local Title    = "Luacheck in editor"
-local Info = { --luacheck: no unused
-  Author        = "Shmuel Zeigerman";
-  Guid          = "DD595F2F-CCB2-4188-BCD0-B1AB98B80DB6";
-  MinFarVersion = "3.0.3300";
-  Started       = "2020-09-13";
-  Title         = Title;
-}
 -------- /Settings
+
+local dirsep = package.config:sub(1,1)
+local osWin = (dirsep == "\\")
 
 local OptFileName = ".luacheckrc"
 local MenuMaxHeight = 5
 
 local F = far.Flags
 
-local menuflags = bit64.bor(F.FMENU_SHOWAMPERSAND,F.FMENU_WRAPMODE)
---if FarBuild >= 5505 then
---  menuflags = bit64.bor(menuflags, F.FMENU_SHOWSHORTBOX, F.FMENU_NODRAWSHADOW)
---end
+local menuflags = bit64.bor(F.FMENU_SHOWAMPERSAND, F.FMENU_WRAPMODE,
+  F.FMENU_SHOWSHORTBOX or 0, F.FMENU_NODRAWSHADOW or 0)
 
 local function traverse(path)
+  local pattern = ("^(.*SEP).-SEP"):gsub("SEP", dirsep)
   return function(_, curpath)
-    return curpath:match("^(.*/).-/")
-  end, nil, path.."/"
+    return curpath:match(pattern)
+  end, nil, path..dirsep
 end
 
 -- @param field : "globals", "read_globals", etc., i.e. fields used in .luacheckrc files
@@ -39,6 +44,14 @@ local function merge_field(field, ...)
   return trg
 end
 
+local function GetConfig()
+  if osWin then
+    return require "far2.luacheck_config"
+  else
+    return dofile(win.JoinPath(far.GetMyHome(), "luacheck_config.lua")) -- luacheck: ignore 143 (undef. field GetMyHome)
+  end
+end
+
 local function GetOptions()
   local options = {}
   local edtname = editor.GetFileName()
@@ -49,7 +62,7 @@ local function GetOptions()
         local env = {}
         setfenv(f, env)("far") -- the argument tells the config. file it is run from Far environment
         if env.luafar or env.lf4ed or env.luamacro then
-          local cfg = dofile( far.GetMyHome().."/luacheck_config.lua" )
+          local cfg = GetConfig()
           local luafar = env.luafar and cfg.luafar
           local lf4 = env.lf4ed and cfg.lf4ed
           local luamacro = env.luamacro and cfg.luamacro
@@ -102,7 +115,7 @@ local function CheckEditor()
     }
 
     local function show_found()
-      local R = actl.GetFarRect()
+      local R = far.AdvControl("ACTL_GETFARRECT")
       props.Y = (R.Bottom-R.Top+1) - (props.MaxHeight+3)
       props.X = (R.Right-R.Left+1) - (maxlen+6)
       local info = items[props.SelectIndex].info
@@ -142,9 +155,13 @@ local function CheckEditor()
   end
 end
 
-Macro {
-  id="80C371AF-8850-428A-A0AA-D8DD166796D8";
-  description=Title;
-  area="Editor"; key="CtrlShiftF7"; filemask="*.lua";
-  action=function() CheckEditor() end;
-}
+if Macro then
+  Macro {
+    id="80C371AF-8850-428A-A0AA-D8DD166796D8";
+    description=Title;
+    area="Editor"; key=MacroKey; filemask="*.lua";
+    action=function() CheckEditor() end;
+  }
+else
+  CheckEditor()
+end
