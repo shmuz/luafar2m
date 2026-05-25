3b21f8b6ebe8f908051b1439c101b44fde8f1c02
Shmuel Zeigerman <solomuz0@gmail.com>
2026-05-25 18:18:59 +0300

    Refactor macro-tests

--- Macros/scripts/Shell/macrotest.lua
+++ Macros/scripts/Shell/macrotest.lua
@@ -1,3 +1,6 @@
+local MacroKeys = { "CtrlShiftF12", "RCtrlShiftF12" }
+local CommonKey = MacroKeys[1]
+
 local F = far.Flags
 local State
 
@@ -10,7 +13,9 @@ local function select_tests(mod)
 
   local t = {}
   for nm in pairs(mod) do
-    if nm ~= "test_all" then t[#t+1] = nm; end
+    if nm ~= "test_all" and nm ~= "SetMacroKeys" then
+      t[#t+1] = nm;
+    end
   end
   table.sort(t, function(a,b) return utf8.ncasecmp(a,b) < 0 end)
   for _, nm in ipairs(t) do
@@ -19,7 +24,7 @@ local function select_tests(mod)
   end
 
   table.insert(items, {tp="sep"})
-  table.insert(items, {tp="butt"; text="&Run"; centergroup=1; default=1; })
+  table.insert(items, {tp="butt"; text="&Run"; centergroup=1; default=1; name="run"; })
   table.insert(items, {tp="butt"; text="&Clear"; centergroup=1;  btnnoclose=1; y1=""; name="clear" })
   table.insert(items, {tp="butt"; text="&Invert"; centergroup=1; btnnoclose=1; y1=""; name="invert" })
 
@@ -27,13 +32,13 @@ local function select_tests(mod)
   local Pos = Dlg:Indexes()
 
   items.proc = function(hDlg, msg, p1, p2)
-    if msg==F.DN_BTNCLICK then
-      for i=1,#items do
-        if items[i].tp=="chbox" then
-          if p1==Pos.clear then hDlg:SetCheck(i, false)
-          elseif p1==Pos.invert then hDlg:SetCheck(i, hDlg:GetCheck(i)==0)
-          end
+    if msg == F.DN_BTNCLICK then
+      if p1 == Pos.clear or p1 == Pos.invert then
+        local flag = (p1 == Pos.clear) and F.BSTATE_UNCHECKED or F.BSTATE_TOGGLE
+        for i=1,#items do
+          if items[i].tp == "chbox" then hDlg:SetCheck(i,  flag); end
         end
+        hDlg:SetFocus(Pos.run)
       end
     end
   end
@@ -54,8 +59,10 @@ local function test_macroengine(interactive, verbose)
   local WaitMsg = function() far.Message("Please wait...", "Test macro engine", "") end
   local PassMsg = function() far.Message("PASS", "Macro engine tests") end
 
+  mf.AddExitHandler(panel.SetPanelDirectory, nil, 1, panel.GetPanelDirectory(nil,1))
   Far.DisableHistory(0x0F)
   local mod = require "far2.test.macrotest"
+  mod.SetMacroKeys(unpack(MacroKeys))
   if interactive then
     local tests = select_tests(mod)
     if tests then
@@ -143,7 +150,7 @@ end
 Macro {
   id="B7B0B120-A616-4CBF-AB83-34E2EC024083";
   description="Test macro engine";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=59;
   action = function() test_macroengine(false,true) end;
 }
@@ -151,7 +158,7 @@ Macro {
 Macro {
   id="C003453B-A808-4BE3-B384-3E9BB732D4AC";
   description="Test macro engine (interactive)";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=10;
   action = function() test_macroengine(true,true) end;
 }
@@ -159,7 +166,7 @@ Macro {
 Macro {
   id="FBD67701-0158-4191-8C4F-A5C174680BEB";
   description="Test plugin Polygon";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=57;
   action = function() test_polygon(true) end;
 }
@@ -167,7 +174,7 @@ Macro {
 Macro {
   id="38147368-BE34-4556-840C-D2837A91C129";
   description="Test plugin LF Search";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=58;
   action = function() test_lfsearch(true) end;
 }
@@ -175,7 +182,7 @@ Macro {
 Macro {
   id="F5922F72-C7F8-4E53-A95F-AA86270B124D";
   description="Test plugin Sqlarc";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=50;
   action = function() test_sqlarc(true) end;
 }
@@ -184,7 +191,7 @@ if jit then
   Macro {
     id="FC08C387-41CE-47CC-9D43-05E775E6CC3C";
     description="Test Hex Editor";
-    area="Shell"; key="CtrlShiftF12";
+    area="Shell"; key=CommonKey;
     sortpriority=40;
     action = function() test_hexed(true) end;
   }
@@ -193,7 +200,7 @@ end
 Macro {
   id="0BD88546-AA06-4569-A80D-0E9EEAC2CB7B";
   description="Test farapi.lua";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=15;
   action=function() test_farapi_lua(true) end;
 }
@@ -201,7 +208,7 @@ Macro {
 Macro {
   id="5838EF38-4EB6-4104-BD41-EE80124827E6";
   description="Test ALL";
-  area="Shell"; key="CtrlShiftF12";
+  area="Shell"; key=CommonKey;
   sortpriority=60;
   action=function()
     test_macroengine(false,false)
