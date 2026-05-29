3ac036d1dabfb677bac6cf3bc99716b36963f72b
Shmuel Zeigerman <solomuz0@gmail.com>
2026-05-29 10:05:31 +0300

    Update diz.lua

--- Macros/scripts/Shell/diz.lua
+++ Macros/scripts/Shell/diz.lua
@@ -1,8 +1,35 @@
--- started: 2026-05-27
--- Multi-line "Describe files" dialog
+-- Started:      2026-05-27
+-- Author:       Shmuel Zeigerman
+-- Purpose:      "Describe files" functionality with a multi-line dialog editor
+-- Portability:  far2m (only)
+-- Plugin:       LuaMacro (only)
 
 local MacroKey = "CtrlShiftZ"
--- local F = far.Flags
+
+local Eng = {
+  Title       = "Describe file";
+  EnterDescr  = "Enter description for";
+  ButtWrite   = "&1 Write";
+  ButtSkip    = "&2 Skip";
+  ButtDelete  = "&3 Delete";
+  ButtCancel  = "&4 Cancel";
+  Error       = "Error";
+  ErrOpenFile = "Could not open file";
+}
+
+local Rus = {
+  Title       = "Описание файла";
+  EnterDescr  = "Введите описание для";
+  ButtWrite   = "&1 Записать";
+  ButtSkip    = "&2 Пропустить";
+  ButtDelete  = "&3 Удалить";
+  ButtCancel  = "&4 Отмена";
+  Error       = "Ошибка";
+  ErrOpenFile = "Ошибка открытия файла";
+}
+
+local F = far.Flags -- luacheck:ignore (unused)
+local M = Eng
 
 local OP_WRITE, OP_DELETE, OP_SKIP = 1,2,3
 local Utf8Bom = "\239\187\191"
@@ -20,23 +47,31 @@ end
 
 local function GetDiz(fname, diz)
   local sd = require "far2.simpledialog"
-  local Prompt = ("Enter description for \"%s\":"):format(fname)
+  local Prompt = ("%s \"%s\":"):format(M.EnterDescr, fname)
   local Items = {
     guid = "3371CF06-7F0B-45DF-8591-7CFE53E2DAC7";
     -- help = "Contents";
     -- width = 76;
-    { tp="dbox"; text="Describe file"; },
+    { tp="dbox"; text=M.Title; },
     { tp="text"; text=Prompt; },
     { tp="memo"; height=5; val=diz; name="memo"; },
     { tp="sep" },
-    { tp="butt"; centergroup=1; text="&1 Write";  name="write";  default=1; },
-    { tp="butt"; centergroup=1; text="&2 Skip";   name="skip"; },
-    { tp="butt"; centergroup=1; text="&3 Delete"; name="delete"; },
-    { tp="butt"; centergroup=1; text="&4 Cancel"; cancel=1; },
+    { tp="butt"; centergroup=1; text=M.ButtWrite;  name="write";  default=1; },
+    { tp="butt"; centergroup=1; text=M.ButtSkip;   name="skip"; },
+    { tp="butt"; centergroup=1; text=M.ButtDelete; name="delete"; },
+    { tp="butt"; centergroup=1; text=M.ButtCancel; cancel=1; },
   }
 
   local Dlg = sd.New(Items)
   local Pos = Dlg:Indexes()
+
+  ---- Uncomment this if you want the Tab key to be handled by the editor.
+  -- function Items.proc(hDlg, Msg, Param1, Param2)
+  --   if Msg == "EVENT_KEY" and Param1 == Pos.memo and Param2 == "Tab" then
+  --     editor.ProcessKey(nil, F.KEY_TAB)
+  --     return true -- tell Far the key was processed
+  --   end
+  -- end
 
   local out, pos = Dlg:Run()
   if out then
@@ -75,7 +110,7 @@ function DizList:Read()
   local DizData, Map = self.DizData, self.Map
   local Record = nil
   local CurDir = panel.GetPanelDirectory(nil,1).Name
-  local FileList = Far.GetConfig("Descriptions.ListNames")
+  local FileList = Far.GetConfig("Descriptions.ListNames") -- via Macro API
 
   for str in FileList:gmatch("[^,]+") do
     local fname = win.JoinPath(CurDir, str)
@@ -130,7 +165,7 @@ function DizList:Flush()
 
   local fp = io.open(self.DizFileName, "w")
   if not fp then
-    far.Message("Could not open file", "Error", nil, "w")
+    far.Message(M.ErrOpenFile, M.Error, nil, "w")
     return
   end
 
@@ -154,10 +189,15 @@ end
 function DizList:DescribeFiles()
   self:Read()
 
+  local Items = {}
   local API = panel.GetPanelInfo(nil,1)
-  for i=1,API.SelectedItemsNumber do
-    local item = panel.GetSelectedPanelItem(nil,1,i)
 
+  -- collect selected items in advance to make possible unselect them one by one
+  for i=1,API.SelectedItemsNumber do
+    table.insert(Items, panel.GetSelectedPanelItem(nil,1,i))
+  end
+
+  for _,item in ipairs(Items) do
     local obj = self.Map[item.FileName]
     if obj == nil then
       obj = { FileName=item.FileName; Diz=""; }
@@ -173,15 +213,18 @@ function DizList:DescribeFiles()
       self.Map[obj.FileName] = nil
       obj.Deleted = true
       self:Flush()
-    elseif op == OP_SKIP then -- luacheck:ignore
-      -- empty if branch
+    elseif op == OP_SKIP then -- luacheck:ignore (empty if branch)
+      -- no action needed
     else
-      break -- canceled
+      break -- canceled by the user
     end
+
+    Panel.Select(0,0,2,obj.FileName) -- unselect (via Macro API)
   end
 end
 
 local function main()
+  M = win.GetEnv("FARLANG")=="Russian" and Rus or Eng
   local MyDizList = NewDizList()
   MyDizList:DescribeFiles()
 end
@@ -192,6 +235,6 @@ Macro {
   flags="NoPluginPanels";
   area="Shell"; key=MacroKey;
   action=function()
-    main()
+    mf.acall(main)
   end;
 }
