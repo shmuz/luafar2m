2129d26472fadb87065f0e8c1e6916dde876eb2f
Shmuel Zeigerman <solomuz0@gmail.com>
2026-06-29 20:39:56 +0300

    Minor

--- Plugins/highlight/src/syntaxes/syntax_cpp.lua
+++ Plugins/highlight/src/syntaxes/syntax_cpp.lua
@@ -45,7 +45,7 @@ local syntax_cpp =
       auto|const|double|float|int|short|struct|unsigned|break|continue|else|for|long|signed|switch|
       void|case|default|enum|goto|register|sizeof|typedef|volatile|char|do|extern|if|return|static|
       union|while|asm|dynamic_cast|namespace|reinterpret_cast|try|bool|explicit|new|static_cast|
-      typeid|catch|operator|template|typename|class|friend|private|this|using|const_cast|
+      typeid|catch|operator|template|typename|class|friend|private|this|using|const_cast|override|
       inline|public|throw|virtual|delete|mutable|protected|wchar_t|and|bitand|compl|not_eq|
       or_eq|xor_eq|and_eq|bitor|not|or|xor |__property|__published|__fastcall
       )\b ]];
